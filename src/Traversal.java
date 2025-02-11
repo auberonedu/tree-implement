d6d85b01cc2579f70c50afe45fd1e64a558d42b2
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-02-11 12:51:36 -0800

    day 1

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,21 @@
+
 public class Traversal {
   public static void main(String[] args) {
 
+    TreeNode root = new TreeNode(77, null, null);
+    root.left = new TreeNode(22, null, null);
+    root.right = new TreeNode(86, null, null);
+    root.left.right = new TreeNode(33, null, null);
+
+    preOrder(root);
+  }
+
+  public static void preOrder(TreeNode node) {
+    if(node == null) {
+      return;
+    }
+    System.out.println(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
   }
 }
