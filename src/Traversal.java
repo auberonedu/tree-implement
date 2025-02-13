1d344a08d9fa07137fa8157bc50dfd1c6990fb62
Auberon López <auberon.lopez@greenriver.edu>
2025-02-13 22:48:30 +0000

    day 3

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,10 +1,11 @@
+import java.util.Stack;
 
 public class Traversal {
   public static void main(String[] args) {
 
     TreeNode<Integer> root = new TreeNode<>(77, null, null);
     root.left = new TreeNode<>(22, null, null);
-    root.right = new TreeNode<>(86, null, null);
+    root.right = new TreeNode<>(87, null, null);
     root.right.left = new TreeNode<>(9, null, null);
     root.left.right = new TreeNode<>(33, null, null);
     root.left.right.left = new TreeNode<>(86, null, null);
@@ -13,7 +14,18 @@ public class Traversal {
     stringRoot.left = new TreeNode<>("hi", null, null);
     stringRoot.right = new TreeNode<>("hey", null, null);
 
-    preOrder(stringRoot);
+    // System.out.println(countLevels(root));
+
+    TreeNode<Integer> megaRoot = new TreeNode<>(0, null, null);
+
+    TreeNode<Integer> current = megaRoot;
+
+    for(int i = 1; i <= 999999; i++) {
+      current.left = new TreeNode<>(i, null, null);
+      current = current.left;
+    }
+
+    preOrderIter(root);
 
     // preOrder(root);
     //postOrder(root);
@@ -23,6 +35,29 @@ public class Traversal {
     // System.out.println(result);
   }
 
+  public static <T> void preOrderIter(TreeNode<T> node) {
+    Stack<TreeNode<T>> nodeStack = new Stack<>();
+
+    nodeStack.push(node);
+
+    while(!nodeStack.empty()) {
+      TreeNode<T> current = nodeStack.pop();
+
+      if(current == null) {
+        continue;
+      }
+      System.out.println(current.value);
+      nodeStack.push(current.right);
+      nodeStack.push(current.left);
+    }
+  } 
+
+
+
+
+
+
+
   public static <T> void preOrder(TreeNode<T> node) {
     if(node == null) {
       return;
@@ -31,6 +66,18 @@ public class Traversal {
     preOrder(node.left);
     preOrder(node.right);
   }
+
+
+
+
+
+
+
+
+
+
+
+
 
   public static <T> void postOrder(TreeNode<T> node) {
     if (node == null) return;
@@ -77,5 +124,23 @@ public class Traversal {
     count += countNodes(node.right); // count right subtree
     return count;
 
+  }
+
+  public static <T> int countLevels(TreeNode<T> node) {
+    if(node == null) return 0;
+    // height of larger subtree + 1
+    int leftHeight = countLevels(node.left);
+    int rightHeight = countLevels(node.right);
+    int biggerHeight = Math.max(leftHeight, rightHeight);
+
+    return biggerHeight + 1;
+  }
+
+  // null -> true
+  public static boolean allOdd(TreeNode<Integer> node) {
+    if(node == null) return true;
+    return node.value % 2 != 0 &&
+          allOdd(node.left) &&
+          allOdd(node.right);
   }
 }
